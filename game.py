f38d6f9e0d96687efed7c50ec1aee51f1e0febbb
samuelbounan <samuel.bounan@protonmail.com>
2023-11-05 19:53:43 +0100

    alpha beta ok + logging system

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 from tools import *
 from players import *
 
@@ -12,13 +15,28 @@ class Game:
         self.N_dice = 8
         self.r = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4]
 
+    def reinit(self):
+        self.grill = [True for k in range(self.domino_max-self.domino_min+1)]
+        self.playerA.reinit()
+        self.playerB.reinit()
+
+
+    def score(self, player):
+        if player == 'A':
+            return sum((self.r[domino-self.domino_min]  for domino in self.playerA.dominos))
+        if player == 'B':
+            return sum((self.r[domino-self.domino_min]  for domino in self.playerB.dominos))
+        return ValueError
+
     def __str__(self) -> str:
         res = ''.join(["X" if dom else "." for dom in self.grill])
-        res += '   ' + str(sum(self.playerA.dominos)).ljust(4) + str(sum(self.playerB.dominos))
+        res += '   ' + str(self.playerA.dominos[-1] if self.playerA.dominos else 0).ljust(4)
+        res += str(self.playerB.dominos[-1] if self.playerB.dominos else 0).ljust(4)
         return res
 
     def over(self) -> bool:
         return(not np.any(self.grill))
+    
     
     def play_dice_part(self, player : Player) -> int:
         nb_available_dice = self.N_dice
@@ -27,8 +45,10 @@ class Game:
         
         while 1:
             dice_results = draw_dice(nb_available_dice)
+            logging.info(f"dice: {dice_results}")
             player_choice = player.play_dice(dice_results, previous_choices, nb_available_dice, score)
             if player_choice == -1:
+                logging.info(f"obtains score: {score}")
                 return(score)
             else:
                 nb_available_dice -= dice_results[player_choice]
@@ -37,14 +57,22 @@ class Game:
             
     
     def play_grill_part(self, player : Player, score : int) -> int:
+        logging.info(f"state: {self}")
         player_selection = player.play_grill(self.grill, score)
         return(player_selection)
     
-    def play_a_game(self):
+    def play_game(self, display=True):
+
         playing_player, waiting_player = self.playerA, self.playerB
         
         while not(self.over()):
-            print(self)
+            if display:
+                print(self)
+            if playing_player == playerA:
+                logging.info("A plays")
+            else:
+                logging.info("B plays")
+
             top_domino_waiting_p = waiting_player.dominos[-1] if waiting_player.dominos else 0
             playing_player.init_turn(self.grill,self.r,top_domino_waiting_p)
             player_score = self.play_dice_part(playing_player)
@@ -52,12 +80,14 @@ class Game:
 
             if player_selection == player_score:#stealing is possible
                 if waiting_player.dominos and player_selection == waiting_player.dominos[-1]:
+                    logging.info(f"steals {player_selection}")
                     playing_player.dominos.append(waiting_player.dominos.pop(-1))
                     #new turn
                     playing_player, waiting_player = waiting_player, playing_player
                     continue
             
             if player_selection != -1 and self.grill[player_selection-self.domino_min]:#The playing player has chosen a domino
+                logging.info(f"takes {player_selection}")
                 self.grill[player_selection-self.domino_min] = False
                 playing_player.dominos.append(player_selection) 
                
@@ -65,6 +95,9 @@ class Game:
                 lost_domino = None
                 if playing_player.dominos:
                     lost_domino = playing_player.dominos.pop(-1)
+                    logging.info(f"looses domino {lost_domino}")
+                else:
+                    logging.info("looses turn")
 
                 for i in range(self.domino_max, self.domino_min-1, -1):
                     if self.grill[i-self.domino_min] and i != lost_domino:
@@ -72,17 +105,18 @@ class Game:
                         break
             #new turn
             playing_player, waiting_player = waiting_player, playing_player 
-        
-        final_score_playerA = sum((self.r[domino-self.domino_min]  for domino in self.playerA.dominos))
-        final_score_playerB = sum((self.r[domino-self.domino_min]  for domino in self.playerB.dominos))
-        
-        rep = "DRAW!"
-        if final_score_playerA > final_score_playerB:
-            rep = "A wins"
-        elif final_score_playerB > final_score_playerA:
-            rep = "B wins"
-        
-        print(rep + "|| PlayerA dominos':", self.playerA.dominos, "| PlayerB dominos':", self.playerB.dominos)
+
+        if display:
+            final_score_playerA = self.score("A")
+            final_score_playerB = self.score("B")
+
+            rep = "DRAW!"
+            if final_score_playerA > final_score_playerB:
+                rep = "A wins"
+            elif final_score_playerB > final_score_playerA:
+                rep = "B wins"
+            
+            print(rep + "|| PlayerA dominos':", self.playerA.dominos, "| PlayerB dominos':", self.playerB.dominos)
         
           
 
@@ -91,8 +125,24 @@ class Game:
 
 if __name__ == "__main__":
 
-    playerA = Player(1,1)
-    playerB = Player(1,1)
-    game = Game(playerA, playerB)
-    game.play_a_game()
+    log_path = 'game.log'
+    try:
+        os.remove(log_path)
+    except OSError:
+        pass
+    logging.basicConfig(filename=log_path, level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+    playerA = Player()
+    playerA.set_ab(1,0.1)
+    playerB = Player()
+    playerB.set_ab(1,0.1)
+    game = Game(playerA, playerB)
+    score_A, score_B = 0,0
+    for _ in range(10):
+        game.reinit()
+        game.play_game(display=True)
+        score_A += game.score("A")
+        score_B += game.score("B")
+    print(f"Final scores: A = {score_A}, B = {score_B}")
+
+    logging.shutdown()
--- players.py
+++ players.py
@@ -1,35 +1,51 @@
 import functools
 import time
+import logging
 from tools import *
 
 
 
 class Player:
     
-    def __init__(self, alpha: int, beta:int) -> None:
+    def __init__(self) -> None:
         self.N_dice = 8
         self.N_faces = 6
         self.domino_min = 21
         self.domino_max = 36
         self.C = 0
         self.r = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4]
+        self.adv = (0,0)
         self.dominos = []
+        self.alpha = 1
+        self.beta = 1
+
+    def reinit(self) -> None:
+        self.C = 0
+        self.r = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4]
+        self.adv = (0,0)
+        self.dominos = []
+
+    def set_ab(self, alpha: int, beta:int) -> None:
         self.alpha = alpha
         self.beta = beta
 
     def set_C(self, new_C: int):
+        logging.debug("cache clear")
         self.expectancy.cache_clear()
         self.strategy.cache_clear()
         self.C = new_C
 
     def set_r(self, new_r: list[int]):
+        logging.debug("cache clear")
         self.expectancy.cache_clear()
         self.strategy.cache_clear()
         self.r = new_r.copy()
 
     def rewardfun(self, score : int) -> int:
         if score < self.domino_min:
-            return(self.C)
+            return(-self.C)
+        if score == self.adv[0]:
+            return self.adv[1]
         else:
             return(max(self.r[:min(score - self.domino_min , self.domino_max - self.domino_min) + 1]))
 
@@ -51,20 +67,24 @@ class Player:
         else:
             self.set_C(0)
 
+        # init adv
+        if top_domino_adv > 0:
+            r_adv = r[top_domino_adv-self.domino_min]
+            self.adv = (top_domino_adv, 2*r_adv*self.beta)
+        else:
+            self.adv = (0,0)
+
         # init r
         new_r = self.r.copy()
         for i,available in enumerate(grill):
             if not available:
-                new_r[i] = self.C
-        if top_domino_adv > 0:
-            steal_idx = top_domino_adv-self.domino_min
-            new_r[steal_idx] = 2*r[steal_idx]*self.beta
+                new_r[i] = -self.C
         self.set_r(new_r)
 
 
 
     def play_dice(self, dice_results : tuple, previous_choices : int, nb_available_dice : int, score : int) -> int:
-        """Do the choice when dices are drawn
+        """Do the choice when dice are drawn
 
         :param dice_results: _description_
         :type dice_results: tuple
@@ -77,7 +97,9 @@ class Player:
         :return: _description_
         :rtype: int
         """
-        return(self.strategy(dice_results, previous_choices, nb_available_dice, score)[0])
+        choice, reward = self.strategy(dice_results, previous_choices, nb_available_dice, score)
+        logging.info(f"chooses {choice} with reward {reward}")
+        return(choice)
     
     def play_grill(self, grill, score : int) -> int:
         """Selects the domino according to the score and the grill
@@ -91,6 +113,8 @@ class Player:
         """
         if score < self.domino_min:
             return(-1)
+        if score == self.adv[0]:
+            return score
         domino = np.argmax(self.r[:score-self.domino_min+1]) + self.domino_min
         domino = domino if self.r[domino-self.domino_min] > 0 else -1
         return(domino)
@@ -106,7 +130,7 @@ class Player:
     
     
     @functools.cache
-    def strategy(self, dice_results : tuple, previous_choices : int, nb_available_dice : int, score : int):
+    def strategy(self, dice_results : tuple, previous_choices : int, nb_available_dice : int, score : int) -> (int, int):
         """Computes the optimal strategy
         
         :param dice_results: Results of the dice. dice_results[i] is the number of dice drawing i. 0 is a worm.
@@ -118,7 +142,9 @@ class Player:
         :param score: current_score
         :type score: int
         """
-        reward = self.rewardfun(score) if previous_choices % 2 == 1 else self.C
+        # reward init with score if a picko was stashed o/w -C
+        reward = self.rewardfun(score) if previous_choices % 2 == 1 else -self.C
+
         choice = -1
         possible_choices = [i for i in range(len(dice_results)) if dice_results[i] != 0 and ((previous_choices >> i) & 1) == 0]
         for choice_temp in possible_choices:
@@ -155,6 +181,3 @@ if __name__ == "__main__":
     tic = time.time()
     print(player.strategy(initial_throw, 0, 8, 0))
     print(time.time() - tic)
-    
-
-    
